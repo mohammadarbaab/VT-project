2d0afc857ecf8e4e90224d5edd50a675670173ac
Mohammad Arbaab <kazmiwrite@gmail.com>
2025-10-26 17:53:08 +0530

    ignore secure parts

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage"; // ✅ Add this line
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Firebase configuration (from .env)
 const firebaseConfig = {
-  apiKey: "AIzaSyBCy8_JxGmEiUK8cJ04kOhrtV3mwmpKG8Q",
-  authDomain: "video-trans-app.firebaseapp.com",
-  projectId: "video-trans-app",
-  storageBucket: "video-trans-app.firebasestorage.app",
-  messagingSenderId: "753235743986",
-  appId: "1:753235743986:web:7f0d7d4cd557b6d2712c6b",
-  measurementId: "G-5V7FLECTBG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// ✅ Initialize and export storage
+// Export Firebase storage
 export const storage = getStorage(app);
